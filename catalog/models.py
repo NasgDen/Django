8a98b64cbfce3a8f6c0d680fcb15180c80d03ff1
Denis Safonov <denissafonov_85@mail.ru>
2025-08-12 20:18:39 +0500

    В модуль models.py добавлена модель Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    image = models.ImageField()
+    catagory = models.ForeignKey()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_ut = models.DateTimeField(auto_now=True)
