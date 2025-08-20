f3350a57d6ed387588431da46ebbad5758a188bf
Denis Safonov <denissafonov_85@mail.ru>
2025-08-20 19:46:12 +0500

    В контроллер добавлен класс ProductListView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
 
 from catalog.apps import CatalogConfig
-from catalog.views import add_product, contacts, home, product, success_view
+# from catalog.views import add_product, contacts, product, success_view, ProductListView
+from catalog.views import ProductListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('contacts/', contacts, name="contacts"),
-    path('product/<int:pk>', product, name="product"),
-    path('add_product/', add_product, name="add_product"),
-    path('success/', success_view, name="success"),
+    # path('', home, name="home"),
+    path('', ProductListView.as_view(), name="product"),
+#     path('contacts/', contacts, name="contacts"),
+#     path('product/<int:pk>', product, name="product"),
+#     path('add_product/', add_product, name="add_product"),
+#     path('success/', success_view, name="success"),
 ]
