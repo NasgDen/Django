d8ced2f9bddc74ac64e8bc76d2ff004b8c2ed422
Denis Safonov <denissafonov_85@mail.ru>
2025-08-20 22:07:21 +0500

    app Blog - в контроллере длбавлен класс BlogListView

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from blogs.apps import BlogsConfig
 
+from .views import BlogListView
+
 app_name = BlogsConfig.name
 
 urlpatterns = [
-
+    path('', BlogListView.as_view(), name="blogs"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Blog
+
+
+class BlogListView(ListView):
+
+    model = Blog
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'blogs'
+
