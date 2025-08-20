8804801d260d8e24e4b8c48860ea0dfd0f493e28
Denis Safonov <denissafonov_85@mail.ru>
2025-08-20 21:48:17 +0500

    Модель Blog зарегистрирована в админке

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'content', 'created_at', 'is_published',)
+    list_filter = ('created_at',)
+    search_fields = ('name', 'is_published',)
