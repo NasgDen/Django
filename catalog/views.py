b969e3b5bb048638c6c4c438593a1e36cf3dd29c
Denis Safonov <denissafonov_85@mail.ru>
2025-08-20 20:59:54 +0500

    Добавленена пагинация для страницы home.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,6 @@
-from django.core.paginator import Paginator
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
-# from .forms import ProductForm
 from .models import Contact, Product
 
 
@@ -19,6 +15,7 @@ class ProductListView(ListView):
     model = Product
     template_name = "catalog/home.html"
     context_object_name = "products"
+    paginate_by = 5
 
 
 class ProductDetailView(DetailView):
@@ -35,47 +32,3 @@ class ContactListView(ListView):
     def get_queryset(self):
         return Contact.objects.get()
 
-
-
-# def home(request):
-#     """ Контроллер для отображения страницы home.html """
-#     products = Product.objects.all().order_by('-id')
-#     paginator = Paginator(products, 5)
-#     page_number = request.GET.get('page')
-#     page_object = paginator.get_page(page_number)
-#     # for product in products:
-#     #     print(product.name)
-#     return render(request, "home.html", {"page_object": page_object})
-
-
-# def contacts(request):
-#     """ Контроллер для отображения страницы contacts.html и обработки POST запроса """
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         message = request.POST.get('message')
-#         phone = request.POST.get('phone')
-#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-#     contacts = Contact.objects.get()
-#     return render(request, "contacts.html", {'contacts': contacts})
-#
-#
-# def product(request, pk):
-#     """ Контроллер для отображения страницы с подробным описанием товара """
-#     product = Product.objects.get(id=pk)
-#     return render(request, 'product.html', {'product': product})
-#
-#
-# def add_product(request):
-#     """ Контроллер для добавления товаров в базу данных """
-#     if request.method == 'POST':
-#         form = ProductForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return render(request, 'success.html')
-#     else:
-#         form = ProductForm()
-#     return render(request, 'add_product.html', {'form': form})
-#
-#
-# def success_view(request):
-#     return render(request, 'success.html')
