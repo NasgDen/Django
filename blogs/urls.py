e73dccd6485e9c29d711d1e93120a276fad9c32a
Denis Safonov <denissafonov_85@mail.ru>
2025-08-21 20:27:22 +0500

    Blog - Создан класс BlogCreateView

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from blogs.apps import BlogsConfig
 
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView
 
 app_name = BlogsConfig.name
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="blogs"),
+    path('blog_create/', BlogCreateView.as_view(), name='blog_create')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Blog
 
 
 class BlogListView(ListView):
+    """ Класс реализующий интерфейс для отображения блогов """
 
     model = Blog
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
 
+
+class BlogCreateView(CreateView):
+    """ Класс реализующий интерфейс для создания блога """
+
+    model = Blog
+    fields = ['name', 'content', 'image', 'is_published', 'count_views']
+    template_name = 'blog/blog_create.html'
+    success_url = reverse_lazy("blogs:blogs")
