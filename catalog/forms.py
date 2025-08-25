63fcc3196f34be15ff9b3391aa5bf10e5aa4d9cd
Denis Safonov <denissafonov_85@mail.ru>
2025-08-25 21:04:11 +0500

    Реализована валидация на запрещенные слова отдельно для имени и отдельно для описания.

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,32 @@ from django import forms
 
 from .models import Product
 
+FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+
 class ProductForm(forms.ModelForm):
+    """ Класс реализующий интерфейс формы для продукта """
     class Meta:
         model = Product
         fields = ['name', 'description', 'image', 'category', 'price', ]
+
+    def clean_name(self):
+        """ Метод проверяет валидацию поля name на запрещенные слова """
+
+        name = self.cleaned_data.get('name')
+        for forbidden_word in FORBIDDEN_WORDS:
+            if forbidden_word in name.lower():
+                self.add_error('name', 'Поле "Имя" не должно содержать запрещенные слова')
+        return name
+
+    def clean_description(self):
+        """ Метод проверяет валидацию поля description на запрещенные слова """
+
+        description = self.cleaned_data.get('description')
+        for forbidden_word in FORBIDDEN_WORDS:
+            if forbidden_word in description.lower():
+                self.add_error('name', 'Поле "Описание" не должно содержать запрещенные слова')
+        return description
+
+
+
