8c87958a7264d66d218bc16f70f13aba7b3dd17d
Denis Safonov <denissafonov_85@mail.ru>
2025-09-03 21:46:57 +0500

    Реализована отправка письма на почту при регистрации

--- config/settings.py
+++ config/settings.py
@@ -109,3 +109,12 @@ LOGIN_REDIRECT_URL = 'catalog:products'
 LOGOUT_REDIRECT_URL = 'catalog:products'
 
 LOGIN_URL = 'users:login'
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
--- users/views.py
+++ users/views.py
@@ -1,9 +1,28 @@
+from django.contrib.auth import login
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
+from config.settings import EMAIL_HOST_USER, EMAIL_USE_TLS, EMAIL_USE_SSL
 
 
 class RegistrationView(CreateView):
     template_name = 'users/registration.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('catalog:products')
+
+    def form_valid(self, form):
+        print(EMAIL_HOST_USER)
+        print(EMAIL_USE_TLS)
+        print(EMAIL_USE_SSL)
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать в наш интернет магазин'
+        message = 'Спасибо, за регистрацию в нашем интернет магазине!'
+        from_email = EMAIL_HOST_USER
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
