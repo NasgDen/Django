f0ac95155109c35f23479dfdcb20b94ce98e90d8
Your Name <you@example.com>
2025-09-13 17:43:27 +0500

    Настройка redis в settings.py

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,10 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+CASHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.radis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
